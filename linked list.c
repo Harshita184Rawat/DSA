635ea272217b70476537dd84ac43fd5b10aacd02
Harshita Rawat <harshita.reenu184@gmail.com>
2023-01-14 22:24:42 +0530

    Update linked list.c

--- linked list.c
+++ linked list.c
@@ -1,16 +1,16 @@
-#include<sdtio.h>
+#include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
 struct node
 {
     int info;
     struct node *next;
-}
-struct node *start==NULL;
+};
+struct node *start=NULL;
 void insert();
 void delete();
 void display();
-int main()
+void main()
 {
     int ch;
     printf("$$ LINKED LIST $$\n");
@@ -25,22 +25,110 @@ int main()
         switch (ch)
         {
         case 1:insert();
-            break;
+                break;
         case 2:delete();
-            break;
+                break;
         case 3:display();
-            break;
+                break;
         case 4:printf("---EXIT----\n!! BYE BYE !!\n");
-            exit(main);
         default: printf("Invalid operation\n");
         }
     }
+    
 }
 void insert()
 {
-    struct node *p==NULL;
+    struct node *p=NULL;
+    struct node *pt=NULL;
     p=(struct node*)malloc(sizeof(struct node));
-    
+    printf("Enter the value to be inserted");
+    scanf("%d",&p->info);
+    if(start==NULL)
+    {
+        start=p;
+    }
+    else
+    {
+        int pos=0;
+        printf("Enter the position");
+        scanf("%d",&pos);
+        if(pos==1)
+        {
+            start=p;
+            p->next=start;
+        }
+        else 
+        {
+            pt=start;
+            for(int i=0;i<pos-1;i++)
+            pt=pt->next;
+            if(pt==NULL)
+            {
+                printf("value of that position can't inserted");
+                free(p);
+            }
+            else
+            {
+                p->next=pt->next;
+                pt->next=p;
+            }
+        }
+    }
+}
 
+void del()
+{
+    struct node *p=NULL;
+    struct node *pt=NULL;
+    if(start==NULL)
+        printf("Underflow List id empty");
+    else
+    {
+        int pos=0, i=0;
+        printf("Enter the position of element to be deleted");
+        scanf("%d",&pos);
+        if(pos==1)
+            {
+                p=start;
+                start=start->next;
+                free(p);
+            }
+        else
+        {
+            p=NULL;
+            pt=start;
+            for(i=0;i<pos;i++)
+            {
+                p=pt;
+                pt=pt->next;
+                if(pt==NULL)
+                    break;
+            }
+            
+            if(pt==NULL)
+                printf("Invalid value");
+            else
+            {
+                p->next=pt->next;
+                free(p);
+            }
+        }
+        
+    }
+}
 
+void display()
+{
+    struct node *p=start;
+    int i=0;
+    if(start==NULL)
+        printf("Empty List");
+    else
+    {
+        while(p!=NULL)
+        {
+            printf("%d\t",p->info);
+            p=p->next;
+        }
+    }
 }
